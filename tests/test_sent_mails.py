9f80b2b5192bd202012aa4eb02a2ba66a2c4fe6b
Christian Hettlage <hettlage@saao.ac.za>
2020-09-15 10:41:30 +0200

    Added tests to implement

---
+++ tests/test_sent_mails.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.mark.skip(reason="not implemented yet")
+def test_register_sent_mail():
+    pass
+
+
+@pytest.mark.skip(reason="not implemented yet")
+def test_an_empty_list_is_returned_if_no_mail_has_been_sent():
+    pass
+
+
+@pytest.mark.skip(reason="not implemented yet")
+def test_sent_mails_are_returned_ordered_by_datetime():
+    pass
+
+
+@pytest.mark.skip(reason="not implemented yet")
+def test_database_is_not_recreated():
+    pass
