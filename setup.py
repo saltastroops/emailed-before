5972f167fa9b34c2d7528f4166e92f8597eb3a1b
Christian Hettlage <hettlage@saao.ac.za>
2020-09-22 16:55:42 +0200

    Use README as package description

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
+import os
 from setuptools import setup, find_packages
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="emailed-before",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     package_data={"emailedbefore": ["py.typed"]},
     packages=find_packages(where="src"),
     package_dir={"": "src"},
@@ -10,5 +18,5 @@ setup(
     install_requires=[
     ],
     tests_require=["mypy", "pytest", "pytest-cov", "pytest-runner"],
-    version="0.1.1",
+    version="0.1.0",
 )
